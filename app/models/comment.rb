f77a9359d746a4daf099aaa7c11a2685954f0405
Daniel Llano <daniel.llano.a@gmail.com>
2014-12-09 14:41:37 -0500

    Added Comments validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,10 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
+
+  validates :commenter, presence: true,
+										length: {minimum: 3}
+
+  validates :body, presence: true,
+										length: {minimum: 2}
+
 end
