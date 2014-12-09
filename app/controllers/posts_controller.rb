e118108d6d23590c85dfafd6fdc2374db2fe111f
Daniel Llano <daniel.llano.a@gmail.com>
2014-12-09 14:18:33 -0500

    Added modal to edit function and customized show view and comments with bootstrap

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,7 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
+    @titulo = "Nuevo Post"
   end
 
   def create
@@ -23,12 +24,13 @@ class PostsController < ApplicationController
 
   def edit
     @post = Post.find(params[:id])
+    @titulo = "Editar Post"
   end
 
   def update
     @post = Post.find(params[:id])
     if @post.update(post_params)
-      redirect_to @posts
+      redirect_to posts_path
     else
       render 'edit'
     end
